d7a9880fbdebd439ccbf1f17316f78d754e97f5b
Dinis Cruz <dinis.cruz@owasp.org>
2025-11-16 15:41:33 +0000

    updated version of osbot-fast-api-serverelss

--- mgraph_ai_service_semantic_text/config.py
+++ mgraph_ai_service_semantic_text/config.py
@@ -3,4 +3,4 @@ from mgraph_ai_service_semantic_text import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph AI Service Semantic_Text"
 FAST_API__DESCRIPTION                    = "Service that transform text using semantic graphs"
-LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.27.0']
+LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['osbot-fast-api-serverless==v1.28.0']
