dddad90a265abddf69ce2cc94c36c3215a09cac8
Dinis Cruz <dinis.cruz@owasp.org>
2025-11-16 16:08:25 +0000

    fixed bug in Schema__Topic_Filter__Response

--- mgraph_ai_service_semantic_text/schemas/topic/Schema__Topic_Filter__Request.py
+++ mgraph_ai_service_semantic_text/schemas/topic/Schema__Topic_Filter__Request.py
@@ -1,4 +1,5 @@
 from typing                                                                                             import Dict, List
+from osbot_aws.aws.comprehend.schemas.safe_str.Safe_Str__AWS_Comprehend__Text                           import Safe_Str__Comprehend__Text
 from osbot_utils.type_safe.Type_Safe                                                                    import Type_Safe
 from osbot_utils.type_safe.primitives.domains.cryptography.safe_str.Safe_Str__Hash                      import Safe_Str__Hash
 from mgraph_ai_service_semantic_text.schemas.classification.enums.Enum__Classification__Logic_Operator  import Enum__Classification__Logic_Operator
@@ -8,7 +9,7 @@ from mgraph_ai_service_semantic_text.schemas.topic.safe_float.Safe_Float__Topic_
 
 
 class Schema__Topic_Filter__Request(Type_Safe):                                # Request to filter hashes by topic criteria
-    hash_mapping            : Dict[Safe_Str__Hash, str]                        # Hash → original text mapping
+    hash_mapping            : Dict[Safe_Str__Hash, Safe_Str__Comprehend__Text] # Hash → original text mapping
     required_topics         : List[Enum__Classification__Topic]                # List of topics that must match
     min_confidence          : Safe_Float__Topic_Confidence                     # Minimum confidence threshold (0.0-1.0)
     logic_operator          : Enum__Classification__Logic_Operator
